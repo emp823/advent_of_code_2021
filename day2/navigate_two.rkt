d86b307dfb10e787ec2667877362c9b0c867557c
Erik Pearson <emp823@gmail.com>
2021-12-26 20:45:31 -0500

    Day 2, part 2 fails

--- day2/navigate_two.rkt
+++ day2/navigate_two.rkt
@@ -9,17 +9,35 @@
         (define steps (string->number (caddr match)))
         `(,direction ,steps)))))
 
-(define (sumValues lst)
+(define (sum-values lst)
   (apply + (map cadr lst)))
 
-(define (filterDir instr dir)
-  (filter (lambda (pair) (eq? (car pair) dir)) instr))
+(struct sub-position (aim depth horiz))
 
-(define fwd (sumValues (filterDir instructions 'forward)))
-(define down (sumValues (filterDir instructions 'down)))
-(define up (sumValues (filterDir instructions 'up)))
+(define (match-move command position)
+  (match command
+    [(list 'forward n) (move-forward n position)]
+    [(list 'up n) (move-up n position)]
+    [(list 'down n) (move-down n position)]))
 
-(define horiz fwd)
-(define depth (- down up))
+(define (move-down n position)
+  (struct-copy sub-position position
+               [aim (+ (sub-position-aim position) n)]))
 
-(* horiz depth)
+(define (move-up n position)
+  (struct-copy sub-position position
+               [aim (- (sub-position-aim position) n)]))
+
+(define (move-forward n position)
+  (struct-copy sub-position position
+               [horiz (+ (sub-position-horiz position) n)]
+               [depth (* (sub-position-aim position) n)]))
+
+(define (navigate)
+  (for/fold ([p (sub-position 0 0 0)]
+            #:result (* (sub-position-horiz p)
+                        (sub-position-depth p)))
+            ([move (in-list instructions)])
+    (match-move move p)))
+
+(navigate)
